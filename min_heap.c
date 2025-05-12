4d199ba5e9705b0a100efc88a3c21446b506eceb
zahrarizkyna24 <zahrarizkyna461@gmail.com>
2025-05-12 20:46:58 +0700

    Update min_heap.c

--- min_heap.c
+++ min_heap.c
@@ -8,3 +8,17 @@ void initMinHeap(MinHeap* heap, int capacity) {
     heap->capacity = capacity;
     heap->tasks = malloc(capacity * sizeof(Task));
 }
+void heapInsert(MinHeap* heap, Task task) {
+    // Jika heap penuh, gandakan kapasitasnya dan realokasi memori
+    if (heap->size == heap->capacity) {
+        heap->capacity *= 2;
+        heap->tasks = realloc(heap->tasks, heap->capacity * sizeof(Task));
+    }
+    // Tambahkan tugas baru di akhir heap, lalu naikkan ke posisi yang tepat berdasarkan days_left
+    int i = heap->size++;
+    while (i > 0 && heap->tasks[(i - 1) / 2].days_left > task.days_left) {
+        heap->tasks[i] = heap->tasks[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+    heap->tasks[i] = task;
+}
